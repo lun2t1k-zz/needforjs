60dc6befbd614710ea0fb8812bfec2192610d292
Dmitry Krutkin <84284545+lun2t1k@users.noreply.github.com>
2021-06-24 21:17:59 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const score = document.querySelector('.score'),
   start = document.querySelector('.start'),
   gameArea = document.querySelector('.gameArea'),
   car = document.createElement('div');
+const music = document.createElement('embed');
+
+music.src = 'GHOSTFACE PLAYA - SWAGGIN AT THE PARTMENT.mp3';
 
 car.classList.add('car');
 
@@ -19,7 +22,7 @@ const keys = {
 const setting = {
   start: false,
   score: 0,
-  speed: 3,
+  speed: 20,
   traffic: 3
 }
 
@@ -29,6 +32,9 @@ function getQuantityElements(heightElement){
 
 function startGame(){
   start.classList.add('hide');
+  gameArea.innerHTML = '';
+  document.body.append(music);
+  music.classList.add('visually-hidden');
 
   for (let i = 0; i < getQuantityElements(100); i++){
     const line = document.createElement('div');
@@ -44,12 +50,16 @@ function startGame(){
     enemy.y = -100 * setting.traffic * (i + 1);
     enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
     enemy.style.top = enemy.y + 'px';
-    enemy.style.background = 'transparent url(./image/enemy2.png) center / cover no-repeat';
+    enemy.style.background = 'transparent url(./image/enemy.png) center / cover no-repeat';
     gameArea.appendChild(enemy);
   }
 
+  setting.score = 0;
   setting.start = true;
   gameArea.appendChild(car);
+  car.style.left = '125px';
+  car.style.top = 'auto';
+  car.style.bottom = '10px';
   setting.x = car.offsetLeft;
   setting.y = car.offsetTop;
   requestAnimationFrame(playGame);
@@ -57,19 +67,21 @@ function startGame(){
 
 function playGame(){
   if (setting.start){
+    setting.score += setting.speed;
+    score.innerHTML = 'SCORE<br>' + setting.score;
     moveRoad();
     moveEnemy();
     if (keys.ArrowLeft && setting.x > 0){
-      setting.x -= setting.speed;
+      setting.x -= setting.speed / 2;
     }
     if (keys.ArrowRight && setting.x < (gameArea.offsetWidth - car.offsetWidth)){
-      setting.x += setting.speed;
+      setting.x += setting.speed / 2;
     }
     if (keys.ArrowUp && setting.y > 0){
-      setting.y -= setting.speed;
+      setting.y -= setting.speed / 2;
     }
     if (keys.ArrowDown && setting.y < (gameArea.offsetHeight - car.offsetHeight)){
-      setting.y += setting.speed;
+      setting.y += setting.speed / 2;
     }
     car.style.left = setting.x + 'px';
     car.style.top = setting.y + 'px';
@@ -92,7 +104,7 @@ function moveRoad(){
   lines.forEach(function(line){
     line.y += setting.speed;
     line.style.top = line.y + 'px';
-
+    
     if (line.y >= document.documentElement.clientHeight){
       line.y = -100;
     }
@@ -103,6 +115,20 @@ function moveRoad(){
 function moveEnemy(){
   let enemy = document.querySelectorAll('.enemy');
   enemy.forEach(function(item){
+    let carRect = car.getBoundingClientRect();
+    let enemyRect = item.getBoundingClientRect();
+
+    if (carRect.top <= enemyRect.bottom &&
+      carRect.right >= enemyRect.left &&
+      carRect.left <= enemyRect.right &&
+      carRect.bottom >= enemyRect.top){
+      setting.start = false;
+      console.warn('ДТП');
+      start.classList.remove('hide');
+      start.style.top = score.offsetHeight;
+      music.remove('embed');
+    }
+
     item.y += setting.speed / 2;
     item.style.top = item.y + 'px';
     if (item.y >= document.documentElement.clientHeight){
